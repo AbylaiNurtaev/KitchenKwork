ac9b7ffd7f316c9ce77d20bd1a99a240902e6582
ITP Project <krutyev6@gmail.com>
2024-03-30 14:25:45 +0600

    полностью готов Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from '../../images/logo.jpg'
 import s from './Header.module.sass'
 import { Link, NavLink } from 'react-router-dom'
 import links from '../../links.json'
+import instIcon from '../../images/icons/inst.png'
+import phoneIcon from '../../images/icons/phone.png'
+import telegram from '../../images/icons/telegram.png'
 
 function Header() {
 
+  const [logoSize, setLogoSize] = useState('big')
+
+  const handleScroll = () => {
+    const newSize = window.scrollY > 50 ? 'small' : 'big'
+    setLogoSize(newSize)
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   return (
-    <div className={s.block}>
+    <div className={`${s.block} ${s[logoSize]}`}>
       <div className={s.innerBlock}>
         <div className={s.logo}>
-          <img src={logo} alt="logo" />
+          <img src={logo} alt="logo" className={`${s.logoImage} ${s[logoSize]}`}/>
         </div>
 
         <div className={s.navigation}>
@@ -31,6 +48,14 @@ function Header() {
             })
           }
 
+        </div>
+
+
+        <div className={s.contacts}>
+          <img className={s.phoneIcon} src={phoneIcon} alt="phone" />
+          <p className={s.phoneNumber}>+7 (778) 464-25-56 </p>
+          <img className={s.instIcon} src={instIcon} alt="inst" />
+          <img className={s.telegramIcon} src={telegram} alt="telegram" />
         </div>
       </div>
     </div>
